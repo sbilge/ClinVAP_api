ac6019d8e6c52390e49dd1578d71834b1b31995b
Bilge Surun <bilgesurun@gmail.com>
2020-05-01 22:17:47 +0200

    nextflow profile is changed

--- nextflow/start_nextflow.py
+++ nextflow/start_nextflow.py
@@ -20,7 +20,7 @@ class MyHandler(FileSystemEventHandler):
         # # call nextflow on new vcf file
         # # change the path according to the VMs folder structure etc. 
         clinvap = subprocess.run(
-            ['nextflow', 'run', 'main.nf', '--vcf', os.path.abspath(event.src_path), '--genome', genome_assembly, '--outdir', DOWNLOADS, '-profile', 'params'], cwd=NEXTFLOW_FOLDER)
+            ['nextflow', 'run', 'main.nf', '--vcf', os.path.abspath(event.src_path), '--genome', genome_assembly, '--outdir', DOWNLOADS, '-profile', 'ApiParameters'], cwd=NEXTFLOW_FOLDER)
 
         # print(event.event_type)
         # print(os.path.abspath(event.src_path))
