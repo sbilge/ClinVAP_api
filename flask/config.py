247cdcb55953fa4462d1b12f3c58d52276a8b930
Bilge Surun <bilgesurun@gmail.com>
2020-05-14 15:53:03 +0200

    minor

--- flask/config.py
+++ flask/config.py
@@ -5,8 +5,8 @@ class Config(object):
     SESSION_COOKIE_SECURE = True
     EXTENSIONS = ["json", "vcf", "docx"]
     SECRET_KEY = "throw-away-key"
-    # UPLOADS = " path/to/the/server"
-    # DOWNLOADS = "path/to/the/server"
+    UPLOADS = "/app/app/static/input/uploads"
+    DOWNLOADS = "/app/app/static/output/downloads"
 
 
 class ProductionConfig(Config):
@@ -15,8 +15,6 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # UPLOADS = "/Users/bilges/Desktop/abi_tuebingen/clinical_reporting/ClinVAP_app/flask/app/static/input/uploads"
-    # DOWNLOADS = "/Users/bilges/Desktop/abi_tuebingen/clinical_reporting/ClinVAP_app/flask/app/static/output/downloads"
     UPLOADS = "/app/app/static/input/uploads"
     DOWNLOADS = "/app/app/static/output/downloads"
     SESSION_COOKIE_SECURE = False
