903793e22c3ae2bda6eec1a7fe952ededc7504b1
Bilge Surun <bilgesurun@gmail.com>
2020-10-27 12:49:32 +0100

    config changed back to original paths

--- flask/config.py
+++ flask/config.py
@@ -6,12 +6,9 @@ class Config(object):
     EXTENSION_SOMATIC = ["json", "vcf"]
     EXTENSION_CNV = ["json", "tsv"]
     SECRET_KEY = "throw-away-key"
-    # UPLOADS = "/app/app/static/input/uploads"
-    UPLOADS = "/Users/bilges/Desktop/abi_tuebingen/clinical_reporting/ClinVAP_app/flask/app/static/input/uploads"
-    # DOWNLOADS = "/app/app/static/output/downloads"
-    DOWNLOADS = "/Users/bilges/Desktop/abi_tuebingen/clinical_reporting/ClinVAP_app/flask/app/static/output/downloads"
-    # NF_CONF = "/app/app/static/input/nf_conf"
-    NF_CONF = "/Users/bilges/Desktop/abi_tuebingen/clinical_reporting/ClinVAP_app/flask/app/static/input/nf_conf"
+    UPLOADS = "/app/app/static/input/uploads"
+    DOWNLOADS = "/app/app/static/output/downloads"
+    NF_CONF = "/app/app/static/input/nf_conf"
 
 class ProductionConfig(Config):
     pass
@@ -19,9 +16,9 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # UPLOADS = "/app/app/static/input/uploads"
-    # DOWNLOADS = "/app/app/static/output/downloads"
-    # NF_CONF = "/app/app/static/input/nf_conf"
+    UPLOADS = "/app/app/static/input/uploads"
+    DOWNLOADS = "/app/app/static/output/downloads"
+    NF_CONF = "/app/app/static/input/nf_conf"
     SESSION_COOKIE_SECURE = False
 
 
@@ -30,7 +27,7 @@ class TestingConfig(Config):
     # TESTING = True
     DEBUG = True
     SESSION_COOKIE_SECURE = False
-    # UPLOADS = "/app/app/static/input/uploads"
-    # DOWNLOADS = "/app/app/static/output/downloads"
-    # NF_CONF = "/app/app/static/input/nf_conf"
+    UPLOADS = "/app/app/static/input/uploads"
+    DOWNLOADS = "/app/app/static/output/downloads"
+    NF_CONF = "/app/app/static/input/nf_conf"
 
